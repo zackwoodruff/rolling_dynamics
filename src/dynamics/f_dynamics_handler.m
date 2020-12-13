dcf9c03b811eafa20c2d456374712c17243ebebe
Zack Woodruff <jzwoodruff@u.northwestern.edu>
2020-12-13 10:07:39 -0800

    Add dynamics function exports, dynamics handler, and open loop sim

---
+++ src/dynamics/f_dynamics_handler.m
@@ -0,0 +1,108 @@
+function [dx] = f_dynamics_handler(t,x,u,tvec_u,rolling_type_num)
+% f_dynamics_handler 
+% This contains the dynamics equations for a hand and an object in rolling
+% contact. The hand is directly controlled, the object is subject to the
+% rolling constraints assuming rolling is maintained.
+
+% Inputs:
+% t = time
+% x = [roll_2, pitch_2, yaw_2; ...                       % Phi_2 (1:3)
+%      s_p2x; s_p2y; s_p2z; ...                          % p (4:6)
+%      u1; v1, u2, v2, psi; ...                          % q (7:11)
+%      b_omega_o2_x; b_omega_o2_y; b_omega_o2_z; ...     % omega_b2 (12:14)
+%      b_vx_o2; b_vy_o2; b_vz_o2                         % vb2 (15:17)
+%      du1; dv1, du2, dv2, dpsi;];                       % dq (18:22)
+% u = dVh_ = [alphax_h_; alphay_h_; alphaz_h_; ax_h_; ay_h_; az_h_]
+% tvec_u: time array corresponding to controls to use for interpolation
+% rolling_type_num: (1) Rolling, (2) Pure Rolling
+
+%Outputs:
+% dx = [ ... Velocities : 
+%      d/dt[roll_2, pitch_2, yaw_2];...                       % dPhi_2(1:3)
+%      s_dp2x; s_dp2y; s_dp2z; ...                            % dp (4:6)
+%      du1; dv1, du2, dv2, dpsi; ...                          % dq (7:11)
+%      ... Accelerations:
+%      b_alpha_o2_x; b_alpha_o2_y; b_alpha_o2_z; ...          % b_alpha_o2 (12:14)
+%      b_dvx_o2; b_dvy_o2; b_dvz_o2                           % b_dv_o2 (15:17)
+%      ddu1; ddv1, ddu2, ddv2, ddpsi;];                       % ddq (18:22)
+
+
+%% Extracting object states from state variable x
+% Configurations
+%Rso2       = reshape(x(1:9),[3,3]); % Object 2 orientation in {s}
+%Phiso2     = x(1:3);
+%pso2       = x(4:6); % Object 2 position in {s}
+q          = x(7:11,:); % contact coordinates
+
+% Velocities
+%omega_b2   = x(12:14); % body rotational velocity object 2
+%vb2        = x(15:17); % body linear velocity object 2
+dq         = x(18:22,:); % contact velocities 
+
+
+%% Extract controls at current time t
+% Feedforward term
+N = length(t);  
+if N == 1
+    b_dV_o2 = interp1(tvec_u,u',t)';
+else
+    b_dV_o2 = u; % for matrix or constant control input
+end
+
+is_single_equation = false;
+if is_single_equation
+    %dx = autoGen_f_full_dynamics_new(t,x,b_dV_o2);
+    warning("NOT DEFINED");
+else
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Hand Dynamics
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Matrix form of hand dynamics
+    ddHand= autoGen_f_hand_dynamics(t,x([1:6,12:17],:),b_dV_o2);
+
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Object Dynamics
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
+    %% Relative Accelerations
+    %rolling_type_num = 1; % (1) Rolling, (2) Pure Rolling
+    alphax=zeros(1,N);
+    alphay=zeros(1,N); 
+    alphaz=zeros(1,N); 
+    for i = 1:N 
+        K5 = autoGen_f_K5(q(:,i));
+        K6 = autoGen_f_K6(x(:,i));
+        Ad_Toh_ = autoGen_f_Ad_Toh(q(:,i));
+        
+        alpha_lambda = inv(K5)*(K6-Ad_Toh_*b_dV_o2(:,i));
+        
+        %C1val = autoGen_fC1exported_new(q(:,i)); 
+        %C2val = autoGen_fC2exported_new(x(:,i), b_dV_o2(:,i)); 
+        %alpha_lambda=inv(C1val)*(C2val);
+        %alpha_lambda=C1val\(C2val);
+        if rolling_type_num == 1 % Rolling
+           alphax(i)=alpha_lambda(1); 
+           alphay(i)=alpha_lambda(2); 
+           alphaz(i)=alpha_lambda(3);
+        elseif rolling_type_num == 2 % Pure Rolling    
+           alphax(i)=alpha_lambda(1); 
+           alphay(i)=alpha_lambda(2);
+           alphaz(i) = autoGen_f_alpha_z_pure_rolling([q(:,i);dq(:,i)]);    %alphaz=0;
+        end
+    end
+
+
+    %% Second Order Kinematics 
+    ddq = autoGen_f_second_order_kinematics(t,[q;dq],[alphax;alphay;alphaz]);
+
+
+    %% Extract States 
+    dx = zeros(22,N); 
+    dx(1:6,:)  = ddHand(1:6,:);
+    dx(7:11,:) = dq; 
+
+    % Accelerations into output array 
+    dx(12:17,:) = ddHand(7:12,:);
+    dx(18:22,:) = ddq; 
+end
--- src/examples/ball_on_plate/main_ball_plate_complete_example.m
+++ src/examples/ball_on_plate/main_ball_plate_complete_example.m
@@ -73,13 +73,97 @@ param.dynamics.gravity = 9.81;
 param = derive_rolling_dynamics(param);
 
 
-toc
+
+%% Export dynamics functions
+directory = pwd; % Export functions to the home directory 
+is_optimize = param.options.is_simplify; % Should we optimize the exported functions? 
+    
+param.options.is_fast_dynamics = true; 
+if param.options.is_fast_dynamics
+    % Export functions for fast dynamics
+    % This method avoids a large symbolic matrix inversion
+    
+    syms t_ real
+    q_ = param.variables.q_;
+    qdq_ = [q_; param.variables.dq_];
+    states_hand_ = param.variables.states_hand_; 
+    states_ = [states_hand_(1:6); param.variables.q_; states_hand_(7:12); param.variables.dq_];
+    controls_ = param.variables.dVh_; 
+        
+    % Second Order Kinematics
+    matlabFunction(subs(param.kinematics.second_order_kinematics_,param.bodies.P_,param.bodies.P),...
+        'File',[directory '\autoGen_f_second_order_kinematics'],'Optimize',is_optimize,...
+        'Outputs', {'dYdt'}, 'Vars',{t_,qdq_,param.variables.Alpha_});
+    
+% Pure Rolling Constraint
+    matlabFunction(param.kinematics.alpha_z_,'File',[directory '\autoGen_f_alpha_z_pure_rolling'],...
+    'Optimize',is_optimize,'Vars',{qdq_});
+    
+    % Hand Dynamics
+    matlabFunction(param.dynamics.dxddx_hand,'file',[directory, '\autoGen_f_hand_dynamics'],...
+        'Optimize',is_optimize,'Vars',{t_, states_hand_, controls_});
+    
+    % Object Dynamics
+    matlabFunction(param.dynamics.K5_,'File',[directory '\autoGen_f_K5'],...
+        'Optimize',is_optimize,'Vars',{q_});
+    matlabFunction(param.dynamics.K6_,'File',[directory '\autoGen_f_K6'],...
+        'Optimize',is_optimize,'Vars',{states_});
+    matlabFunction(param.dynamics.Ad_Toh_,'File',[directory '\autoGen_f_Ad_Toh'],...
+        'Optimize',is_optimize,'Vars',{q_});
+    
+else
+    warning('ENTER FULL DYNAMICS DERIVATION HERE')
+end
+
+
+
 %**********************************************
 % 4. Open Loop Simulation
 %**********************************************
 %% 4.1
 %Simulate using either the full dynamics or the partial dynamics 
 
+% Simulate Dynamic Rolling
+disp('Simulating dynamic rolling...')
+param.options.ode_options = odeset('RelTol',1e-6,'AbsTol',1e-8); % set max step size
+%param.options.ode_options = odeset('RelTol',1e-12,'AbsTol',1e-12); % set max step size
+
+
+% Initial Conditions
+param.options.is_inclined = false; 
+if param.options.is_inclined
+    param.mpp.Xh0 = [0.1; zeros(5,1)];
+else
+    param.mpp.Xh0 = [0; zeros(5,1)];
+end
+param.mpp.q0 = [pi/2; 0; 0; 0; 0];
+
+param.mpp.Vh0 = [0; 0; 7; 0; 0; 0];
+omega_xyz0 = [1;0; -7];
+param.mpp.dq0 = double(subs(param.kinematics.first_order_kinematics_, [param.bodies.P_;param.variables.q_;param.variables.Omega_],...
+                              [param.bodies.P;param.mpp.q0;omega_xyz0]));
+
+
+param.mpp.states0 = [param.mpp.Xh0; param.mpp.q0; param.mpp.Vh0; param.mpp.dq0];
+
+disp('Simulating dynamic rolling...')
+if strcmp(param.options.friction_model,'rolling')
+    rolling_type_num = 1; % (1) Rolling, (2) Pure Rolling
+elseif strcmp(param.options.friction_model,'pure-rolling')
+    rolling_type_num = 2;
+else
+    warning('INVALID FRICTION MODEL TYPE');
+end
+options = param.options.ode_options;
+dt = 0.01; 
+tvec = 0:dt:5;
+tvec_u = tvec;
+controls_t = zeros([6,length(tvec_u)]);
+states0 = param.mpp.states0;
+tic
+[~,states] = ode45(@(t,states) f_dynamics_handler(t,states,controls_t,tvec_u,rolling_type_num), tvec, states0, options);
+toc
+disp('    DONE: Simulating dynamic rolling.')
 
 
 %**********************************************
