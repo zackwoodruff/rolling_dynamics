e4c8407f7afa00129bc19a1aeca6e428dd951a18
Zack Woodruff <jzwoodruff@u.northwestern.edu>
2021-01-05 16:15:04 -0800

    Add check that initial simulation conditions satisfy pure-rolling constraints

--- src/dynamics/run_dynamic_rolling_simulation.m
+++ src/dynamics/run_dynamic_rolling_simulation.m
@@ -1,4 +1,5 @@
 function states_t = run_dynamic_rolling_simulation(param)
+disp('Simulating dynamic rolling...')
 
 % Friction type
 if strcmp(param.options.friction_model,'rolling')
@@ -9,6 +10,14 @@ else
     warning('INVALID FRICTION MODEL TYPE');
 end
 
+% Check pure-rolling assumption isn't violated 
+if friction_model_num ==2
+    if param.sim.omega_xyz0(3)~=0
+        error('Initial conditions violate pure-rolling assumption (omega_z_rel = 0)')
+    end
+end
+
+
 % Run Simulation
 [~,states_t] = ode45(@(t,states) f_dynamics_handler(t,states,...
         param.sim.controls_t,...
@@ -18,4 +27,8 @@ end
         param.sim.tvec,...
         param.sim.states0,...
         param.sim.ode_options);
+
+
+%% Return 
+disp('    DONE.');
 end
