f7684897a952fd7ab255e74acc496068438fbf40
Zack Woodruff <jzwoodruff@u.northwestern.edu>
2021-01-07 18:52:05 -0800

    Add commented out ball plate trajectory comparison to analytic rolling solutions

--- src/examples/ball_on_plate/main_ball_plate_complete_example.m
+++ src/examples/ball_on_plate/main_ball_plate_complete_example.m
@@ -157,6 +157,54 @@ visualize_trajectory(param)
 % - Remove extra scrp code from the end 
 analyze_rolling_trajectory(param, param.sim.states_t)
 
+
+%% Ball on plate case
+% if strcmp(param.geo.model,'plane-sphere')
+%     qdq_t0 = states(1,[7:11,18:22])';
+%     s_X1_t0 = states(1,[1:6])'; 
+%     Tso1_t0 = param.functions.fTso1(qdq_t0,s_X1_t0);
+%     Rso1_t0 = Tso1_t0(1:3,1:3); 
+% 
+% 
+%     a = param.geo.R1; 
+%     M = param.dynamics.inertial.mass1;
+%     I1 = param.dynamics.inertial.I1;
+%     C = I1(1,1);
+%     C_v2 = 2/5*M*a^2;
+%     w = b_omega_o2(1,3);
+%     r1 = [q_t(1,3:4)';0];
+%     V1 = Rso1_t0*Vo_t(4:6,1); 
+%     z = [0,0,1]';
+%     %V1 = w*inv((1+M*a^2*inv(C)))
+%     r0 = r1 - (1 + M*a^2/C)/w * VecToso3(V1)*z
+% 
+%     % T to complete circle = ~14.67 s) 
+%     2*pi/(1/((1 + M*a^2/C)/w))
+% 
+% 
+%     % w/(1+M*a^2/C)*(3+1/3-1)
+%     % M*a^2/C/w*9.81*sin(0.1)
+%     %
+%     x0=0;
+%     y0=0;
+%     omega_c = 2/7*w
+%     period = 2*pi/omega_c
+%     cross(V1,[0;0;w])/w^2
+%     r_c = [x0 - V1(2)/w; y0 - V1(1)/w; 0]
+% 
+% 
+%     % Plot x(t) and y(t) of ball
+%     %o2_p1_t = Rso1_t0' * s_p1_t; 
+% 
+%     Vdrift = 5/2*9.81/w*sin(0.1)
+%     figure(9);  clf; 
+%     plot(s_pco_t(1,:), s_pco_t(2,:),'.')
+%     axis equal
+%     figure(11);  clf; 
+%     plot(t,s_pco_t(1,:)-Vdrift*t, t,s_pco_t(2,:),'.')
+% end
+
+
 %% 
 toc
 
