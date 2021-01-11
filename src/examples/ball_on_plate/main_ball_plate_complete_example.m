b77274d4f066b82cb077cf4231b09c295f68a18a
Zack Woodruff <jzwoodruff@u.northwestern.edu>
2021-01-11 15:26:19 -0800

    Reformat and remove ellipsoid-dish code

--- src/examples/ball_on_plate/main_ball_plate_complete_example.m
+++ src/examples/ball_on_plate/main_ball_plate_complete_example.m
@@ -14,20 +14,25 @@ close all
 tic
 current_example_home_directory = fileparts(matlab.desktop.editor.getActiveFilename);
 cd(current_example_home_directory);
-addpath(genpath('../../'))
+addpath('../../utilities')
+addpath('../../kinematics/')
+addpath('../../dynamics')
+addpath('../../visualization')
+addpath('../../analysis')
 
 
-%**********************************************
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 1. Initialization 
-%**********************************************
-%% 1.1 Initialize:
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 1.1 Initialize:
 %-1.1.1 Rolling Configuration Variables, 
 %-1.1.2 Object Geometry and Inertial
 %-1.1.3 Hand Geometry 
 %-1.1.4 Combined Parameters
 %-1.1.5 Friction Parameters
 param = initialize_ball_plate();
-%param = initialize_ellipsoid_dish();
+
 
 %% 1.2 Input Parameters
 param.options.is_simplify = true;
@@ -35,59 +40,35 @@ param.options.friction_model = 'rolling'; %'pure-rolling' or 'rolling';
 param.options.is_fast_dynamics = true;
 
 param.options.is_inclined = true; % Sets whether plate is tilted or horizontal relative to gravity
-
+param.options.export_directory = current_example_home_directory; 
 % param.options.model = 'ball-plate';
 % param.options.is_generate_figures = false; 
 
 
-%**********************************************
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 2. Derive Kinematics
-%**********************************************
-%% 2.1 Contact Geometry
-% From Appendix B
-disp('Calculating symbolic local contact geometry expressions...')
-
-%-2.1.1 Object Geometry:
-param.kinematics.local_geometry.object = ...
-    derive_local_contact_geometry_expressions(param.bodies.object.fo_,...
-                                              param.variables.Uo_);
-%-2.1.2 Hand Geometry:
-param.kinematics.local_geometry.hand = ...
-    derive_local_contact_geometry_expressions(param.bodies.hand.fh_,...
-                                              param.variables.Uh_);
-                                          
-disp('    DONE.');
-
-
-%% 2.2 First Order Kinematics
-% From Appendix B-B
-% Returns: Omega_, K1_, first_order_kinematics_, omega_rel_fqdq1_
-%          Rpsi_, E1, Ho_tilda_
-
-param = derive_first_order_kinematics(param);
-
-
-%% 2.3 Second Order Kinematics
-% From Appendix B-C
-% Returns: Alpha_, K2_, K3_, second_order_kinematics_, a_roll_, alpha_z_pr_
-% TODO: 
-% - Test alpha_z full derivation with pure-rolling
-% - Improve documentation 
-param = derive_second_order_kinematics(param);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 2.1 Contact Geometry
+% 2.2 First-Order Kinematics
+% 2.3 Second-Order Kinematics
+param  = derive_kinematics(param); 
 
  
- 
-%**********************************************
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 3. Dynamics
-%**********************************************
-%% Derive (3.1) and export (3.2) rolling dynamics
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 3.1 Derive rolling dynamics
+% 3.2 Export dynamics functions
 param = derive_export_dyamics(param);
 
 
-%**********************************************
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 4. Open Loop Simulation
-%**********************************************
-%% 4.1
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 4.1
 % From Section V.C
 %Simulate rolling using either the full dynamics or the partial dynamics 
 
@@ -97,24 +78,15 @@ param.sim.T = 2*pi;
 param.sim.tvec = 0:param.sim.dt:param.sim.T ; % vector of times to export states
 param.sim.ode_options = odeset('RelTol',1e-6,'AbsTol',1e-8); % set numerical integration tolerances
 
-%%
-if 1
-    % Initial Conditions
-    if param.options.is_inclined
-        param.sim.Xh0 = [0.1; zeros(5,1)]; % Initial hand configuration 
-    else
-        param.sim.Xh0 = [0; zeros(5,1)]; % Initial hand configuration 
-    end
-    param.sim.q0 = [pi/2; 0; 0; 0; 0]; % Initial contact coordinates
-    param.sim.Vsh0 = [0; 0; 7; 0; 0; 0]; % Initial hand body twist
-    param.sim.omega_rel0 = [1;0; -7]; % Initial relative rotational velocity
+% Initial Conditions
+if param.options.is_inclined
+    param.sim.Xh0 = [0.1; zeros(5,1)]; % Initial hand configuration 
 else
-    param.sim.Xh0 = [0; zeros(5,1)]; %0.1
-    param.sim.q0 = [pi/2; 0; pi/2; 0; 0];
-    param.sim.Vsh0 = [0; 0; 0; 0; 0; 0];
-    param.sim.omega_rel0 = [-2; 1; 0];       
+    param.sim.Xh0 = [0; zeros(5,1)]; % Initial hand configuration 
 end
-        
+param.sim.q0 = [pi/2; 0; 0; 0; 0]; % Initial contact coordinates
+param.sim.Vsh0 = [0; 0; 7; 0; 0; 0]; % Initial hand body twist
+param.sim.omega_rel0 = [1;0; -7]; % Initial relative rotational velocity
         
 % Calculate initial dq from omega_rel0: 
 param.sim.dq0 = double(subs(param.kinematics.first_order_kinematics_,...
@@ -122,7 +94,7 @@ param.sim.dq0 = double(subs(param.kinematics.first_order_kinematics_,...
             [param.bodies.P;  param.sim.q0;       param.sim.omega_rel0]));
 param.sim.states0 = [param.sim.Xh0; param.sim.q0; param.sim.Vsh0; param.sim.dq0]; % Initial state vector
 
-%% Controls
+% Controls
 param.sim.tvec_u = param.sim.tvec; % time vector for control input
 param.sim.controls_t = zeros([6, length(param.sim.tvec_u)]);
 
@@ -131,10 +103,10 @@ param.sim.states_t = run_dynamic_rolling_simulation(param);
 
 
 
-%**********************************************
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 5. Visualization
-%**********************************************
-%% 5.1 Visualize rolling trajectory 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 5.1 Visualize rolling trajectory 
 
 % Set Options
 param.options.visualization.xlim = [-8,8];
@@ -153,10 +125,10 @@ visualize_trajectory(param)
 
 
 
-%**********************************************
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 6. Analyze Results 
-%**********************************************
-%% 6.1
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 6.1
 % TODO: 
 % - Remove extra scrp code from the end 
 analyze_rolling_trajectory(param, param.sim.states_t)
